bbac39194b9972ba0218d309c781848c21189b50
Analia Mok <analiam319@gmail.com>
2019-03-27 21:56:04 -0400

    Created base style sheet. Stubbed out and styled navbar

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -1,9 +1,14 @@
 <template functional>
-  <nav>
-    <nuxt-link to="/"
-      ><img src="~/assets/images/LightLogo.svg" alt="Nolan Tech"
-    /></nuxt-link>
-    <nuxt-link to="/articles">Articles</nuxt-link>
+  <nav class="nav">
+    <div class="nav__container">
+      <nuxt-link to="/" class="nav__brand"
+        ><img src="~/assets/images/LightLogo.svg" alt="Nolan Tech"
+      /></nuxt-link>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/services">Services</nuxt-link>
+      <nuxt-link to="/work">Work</nuxt-link>
+      <nuxt-link to="/blog">Blog</nuxt-link>
+    </div>
   </nav>
 </template>
 
@@ -11,4 +16,34 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="scss">
+.nav {
+  background-color: $darker-slate;
+  padding: 2rem 0;
+
+  &__container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin: 0 auto;
+    width: 80%;
+
+    a {
+      color: $lightest-slate;
+      display: block;
+      font-weight: 600;
+      font-size: 1.8rem;
+      text-decoration: none;
+
+      &:not(:first-child) {
+        padding: 1.5rem;
+      }
+    }
+  }
+
+  &__brand {
+    max-width: 6rem;
+    margin-right: auto;
+  }
+}
+</style>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   ],
 
   styleResources: {
-    scss: ['assets/scss/_variables.scss']
+    scss: ['assets/scss/_variables.scss', 'assets/scss/_base.scss']
   },
 
   /*
